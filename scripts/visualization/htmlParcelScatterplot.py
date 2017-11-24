883a38954e31bc68044b5d05ced2e643b37e9258
Jessy Bosman <je.bo@live.nl>
2017-11-24 13:38:05 +0100

    Added visualization folder
    + some visualizations using html and python

---
+++ scripts/visualization/htmlParcelScatterplot.py
@@ -0,0 +1,69 @@
+import csv
+import os.path
+
+def readFile(relativePath):
+    ''' Read csv file and return generator of information'''
+    with open(relativePath) as csvfile:
+        reader = csv.DictReader(csvfile)
+        # Return information (as list to remove generator and not able to call)
+
+        return list(reader)
+
+parcelCsv = readFile('../../data/CargoList1.csv')
+
+parcelInfo = []
+for dictionary in parcelCsv:
+    parcelInfo.append((dictionary["parcel_ID"], dictionary["weight (kg)"], dictionary["volume (m^3)"]))
+
+data = []
+for dictionary in parcelCsv:
+    data.append({"x": dictionary["weight (kg)"], "y": dictionary["volume (m^3)"]})
+
+html_str = """
+<html>
+<header>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+
+</header>
+
+<body>
+
+<div style= "width:1000px; height:1000px;">
+<canvas id="parcelScatter"></canvas>
+</div>
+<script>
+var ctx = document.getElementById("parcelScatter").getContext('2d');
+var scatterChart = new Chart(ctx, {
+    type: 'scatter',
+    data: {
+        datasets: [{
+            label: 'Scatter Dataset',
+            fill: false,
+            showLine: false,
+            pointBackgroundColor: 'rgba(255, 99, 132, 0.2)',
+            pointBorderColor: 'rgba(255, 99, 132, 0.8)',
+            pointBorderWidth: 2,
+            data: """ + str(data) + """
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom',
+                display: true,
+                labelString: 'probability'
+            }]
+
+        }
+    }
+});
+</script>
+</body>
+
+</html>
+"""
+
+Html_file= open("parcelScatter.html","w")
+Html_file.write(html_str)
+Html_file.close()
