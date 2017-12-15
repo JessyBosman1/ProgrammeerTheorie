e2ae99ca7923ad3ab40e806a3a8ad4ff52334302
Vincent Damen <vinster1997@gmail.com>
2017-12-15 12:27:33 +0100

    made D

---
+++ scripts/D/hillClimberE.py
@@ -0,0 +1,184 @@
+import random
+import itertools
+import sys
+sys.path.append("..")
+import main
+import SolutionA
+import csv
+
+
+spaceCraftId = main.createObjectsSpaceCraft("DE")
+cargoListId = main.createObjectsCargoList(3)
+
+
+def openResults(filename):
+	"""Haalt de attempt op uit een csv-bestand en returnt de spacelist
+	 en de verdeling"""
+	with open(filename, "r") as file:
+		reader = csv.reader(file)
+		dividedParcels = list(reader)
+
+	spaceList = dividedParcels[0]
+	dividedParcels = dividedParcels[1:]
+	return spaceList, dividedParcels
+
+def getParcels(dividedParcels):
+	"""Vind alle ontbekende parcels die niet in de schepen zitten en returnt
+	deze samen met een lijst van alle gebruikte parcels"""
+	usedParcels = [parcel for ship in dividedParcels for parcel in ship]
+	unusedParcels = [parcel for parcel in cargoListId if parcel not in usedParcels]
+	allParcels = [parcel for parcel in cargoListId]
+	return usedParcels, unusedParcels, allParcels
+
+
+def parcelPicker(usedParcels, amount=10):
+	"""Kiest een bepaald aantal random pakketten"""
+	chosenParcels = []
+
+	for x in range(0, amount):
+		parcel = random.choice(usedParcels)
+		
+		if parcel not in chosenParcels:
+			chosenParcels.append(parcel)
+
+	return chosenParcels
+
+def parcelRemover(chosenParcels, dividedParcels, unusedParcels):
+	"""Haalt parcels uit schepen om opnieuw toe te wijzen"""
+	dividedOutput = []
+	for ship in dividedParcels:
+		newShip = []
+		for parcel in ship:
+			if parcel not in chosenParcels:
+				newShip.append(parcel)
+			else:
+				unusedParcels.append(parcel)
+		dividedOutput.append(newShip)
+	return dividedOutput, unusedParcels
+
+def prepareSpaceCrafts(spaceList, dividedParcels):
+	"""Maakt de schepen klaar om mee te testen"""
+	for ship in range(0,len(spaceList)):
+		shipName = spaceList[ship]
+		spaceCraftId[shipName].reset()
+
+		for parcel in dividedParcels[ship]:
+			spaceCraftId[shipName].addParcelToCraft(cargoListId[parcel].weight, cargoListId[parcel].volume)
+
+def sendToSave(dividedParcels, spaceList, filename):
+	spaceCraftId = main.createObjectsSpaceCraft("DE")
+	cargoListId = main.createObjectsCargoList(3)
+	output = {}
+	output["attempt"] = {}
+	for ship in range(0,len(spaceList)):
+		output["attempt"][spaceList[ship]]={parcel:0 for parcel in dividedParcels[ship]}
+	SolutionA.getBestRun(output, spaceList, filename,True, False, True)
+
+def getScore(dividedAttempt):
+	countParcels = 0
+	for ship in dividedAttempt:
+		countParcels += len(ship)
+	return countParcels
+
+def getShipOrder(spaceList):
+	return sorted(spaceList, key=lambda k: random.random())
+
+def getEfficiency(spaceList):
+	percentageCounter = [0,0]
+	for ship in spaceList:
+		percentageCounter[0] += spaceCraftId[ship].currentPayload / spaceCraftId[ship].maxPayload * 100
+		percentageCounter[1] += spaceCraftId[ship].currentPayloadMass / spaceCraftId[ship].maxPayloadMass * 100
+	percentageCounter[0] = percentageCounter[0]/len(spaceList)
+	percentageCounter[1] = percentageCounter[1]/len(spaceList)
+	return sum(percentageCounter)/2
+
+def hillClimber(filename, saveFile, unusedParcels, removedParcels=5, totalIter=10000, attemptIter=20):
+	"""De hill climber functie zelf, gaat met dmv random toewijzingen opzoek naar verbeteringen"""
+	recordBroken = 0
+	unchanged = 0
+	spaceList, dividedParcels = openResults(filename)
+	if len(unusedParcels)!=0:
+		dividedParcels=unusedParcels
+	runs = 0
+	usedParcels, unusedParcels, allParcels = getParcels(dividedParcels)
+	prepareSpaceCrafts(spaceList,dividedParcels)
+	highEfficiencyScore = getEfficiency(spaceList)
+	while(runs <= totalIter):
+		combinationRuns = 0
+		runs += 1
+		if runs %250 == 0:
+			unchanged += 1
+			print(runs*attemptIter, recordBroken)
+			if unchanged == 10:
+				sendToSave(dividedParcels, spaceList, "BestRuns.csv")
+				return
+				
+		chosenParcels = parcelPicker(usedParcels, removedParcels)
+		#Combination attempt
+		while (combinationRuns <= attemptIter):
+			highScore = getScore(dividedParcels)
+
+			usedParcels, unusedParcels, allParcels = getParcels(dividedParcels)
+			combinationRuns += 1
+			dividedAttempt, unusedAttempt = parcelRemover(chosenParcels, dividedParcels, unusedParcels)
+			prepareSpaceCrafts(spaceList, dividedAttempt)
+			#placing attempt
+			while (len(unusedAttempt) != 0):
+				parcel = random.choice(unusedAttempt)
+				toPlace = True
+				#vind het juiste schip
+				for ship in getShipOrder(spaceList):
+					if spaceCraftId[ship].checkFitCraft(cargoListId[parcel].weight, cargoListId[parcel].volume) != False and toPlace:
+						spaceCraftId[ship].addParcelToCraft(cargoListId[parcel].weight, cargoListId[parcel].volume)
+						dividedAttempt[spaceList.index(ship)].append(parcel)
+						toPlace = False
+
+
+				unusedAttempt.remove(parcel)
+
+			if (getScore(dividedAttempt)>=highScore and highEfficiencyScore>getEfficiency(spaceList)):
+				print(getScore(dividedAttempt), highScore)
+				unchanged = 0
+				sendToSave(dividedAttempt,spaceList, saveFile)
+				highEfficiencyScore=getEfficiency(spaceList)
+				recordBroken += 1
+				dividedParcels = []
+				for x in dividedAttempt:
+					dividedParcels.append(x)
+			elif(getScore(dividedAttempt)>highScore):
+				unchanged = 0
+				print(getScore(dividedAttempt), highScore)
+				sendToSave(dividedAttempt,spaceList, saveFile)
+				highEfficiencyScore=getEfficiency(spaceList)
+				recordBroken += 1
+				dividedParcels = []
+				for x in dividedAttempt:
+					dividedParcels.append(x)
+	print (len(unusedParcels))
+	return unusedParcels;
+
+
+if __name__ == '__main__':
+	spaceCraftId = main.createObjectsSpaceCraft("DE")
+	cargoListId = main.createObjectsCargoList(3)
+	max_runs = 5
+	runs = 0
+	while(runs< max_runs):
+		counter = 0
+		remainingParcels=[]
+
+		while(counter<2):
+			if len(remainingParcels)==0:
+				counter+=1
+			remainingParcels = hillClimber("randomList4.csv","allResultsD.csv", remainingParcels, 5,100000,20)
+			if(remainingParcels != None):
+				print("It's not empty: ",len(remainingParcels))
+				print(remainingParcels)
+			else:
+				counter += 1
+
+		with open("BestRuns.csv", 'a', newline='') as csvfile:
+			spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+			spamwriter.writerow(["end attempt"])
+			csvfile.close()
+		runs +=1
---
+++ scripts/D/randomD.py
@@ -0,0 +1,135 @@
+import csv
+import os.path
+import random
+import itertools
+import sys
+sys.path.append("..")
+import main
+from random import shuffle
+import time
+starttime = time.time()
+
+memoryCount = 77
+
+def randomHelper():
+    # Prepare the spacecraft and cargolist
+    spacecraftobject = main.createObjectsSpaceCraft("DE")
+    cargoobject = main.createObjectsCargoList(3)
+
+    # Make a list with the different spacecrafts
+    spacecrafts = [spacecraft for spacecraft in spacecraftobject.keys()]
+
+    # Create a random order of parcels
+    parcels = [parcel for parcel in cargoobject.keys()]
+    return spacecraftobject, cargoobject, spacecrafts, parcels
+
+def randomAlgorithmD(numberofloops):
+    bestattemptdict = {}
+    bestattemptprice = 1000000000000000000000000000000000000000000000
+    spaceCraftId, cargoListId, spacecraftList, parcelList = randomHelper()
+    for loop in range(numberofloops):
+        # Print loop every now and then to keep user updated
+        if loop % 100 == 0 and loop % 1000 != 0:
+            print("Current loop number:", loop, "Best totalprice = ", bestattemptprice, " while flying ", len(bestattemptdict.keys()), " times" )
+
+        # Randomize the order in de parcellist
+        parcelList = [parcel for parcel in cargoListId.keys()]
+        random.shuffle(parcelList)
+
+        results = {}
+        counter = 0
+        totalprice = 0
+        parcelcheck = 1250
+        for i in range(0,100):
+            counter = i
+            if len(parcelList) != 0:
+                runresult, runlist, runprice = fillSpacecrafts(parcelList, spaceCraftId, cargoListId, spacecraftList)
+                results[counter] =  runresult
+                parcelList = runlist
+                totalprice += runprice
+        #print(totalprice, len(results.keys()))
+        if totalprice < bestattemptprice:
+            bestattemptprice = totalprice
+            bestattemptdict = results
+            print ("<<<< NEW BEST, ", totalprice, " DOLLARS WHILE FLYING ", len(bestattemptdict.keys()), " TIMES >>>>")
+    print ("<<<< BEST FOUND, ", totalprice, " DOLLARS WHILE FLYING ", len(bestattemptdict.keys()), " TIMES >>>>")
+    testlijst = []
+
+    """ Testje om te checken als er iets aangepast wordt
+    for k,v in results.items():
+        for x,y in v.items():
+            if x == "Parcellists":
+                for a,b in y.items():
+                    for item in b:
+                        testlijst.append(item)
+    print(testlijst, len(testlijst))"""
+
+def fillSpacecrafts(parcelList, spaceCraftId, cargoListId, spacecraftList):
+    # Create a copy of the parcellist, because you don't want to loop
+    # through a list while you are removing items from that list
+    copyparcel = [x for x in parcelList]
+    
+    # Reset spacecrafts
+    for spacecraft in spaceCraftId.keys():
+        spaceCraftId[spacecraft].reset()
+
+    # Reset the parcellists and packetcount
+    space0 = []
+    space1 = []
+    space2 = []
+    space3 = []
+    space4 = []
+    space5 = []
+    packetCount = 0
+
+    # Fill the spacecrafts
+    for parcel in copyparcel:
+        parcelList.remove(parcel)
+        if spaceCraftId[spacecraftList[0]].checkFitCraft(cargoListId[parcel].weight, cargoListId[parcel].volume) != False:
+            spaceCraftId[spacecraftList[0]].addParcelToCraft(cargoListId[parcel].weight, cargoListId[parcel].volume)
+            space0.append(parcel)
+            packetCount += 1
+        elif spaceCraftId[spacecraftList[1]].checkFitCraft(cargoListId[parcel].weight, cargoListId[parcel].volume) != False:
+            spaceCraftId[spacecraftList[1]].addParcelToCraft(cargoListId[parcel].weight, cargoListId[parcel].volume)
+            space1.append(parcel)
+            packetCount += 1
+        elif spaceCraftId[spacecraftList[2]].checkFitCraft(cargoListId[parcel].weight, cargoListId[parcel].volume) != False:
+            spaceCraftId[spacecraftList[2]].addParcelToCraft(cargoListId[parcel].weight, cargoListId[parcel].volume)
+            space2.append(parcel)
+            packetCount += 1
+        elif spaceCraftId[spacecraftList[3]].checkFitCraft(cargoListId[parcel].weight, cargoListId[parcel].volume) != False:
+            spaceCraftId[spacecraftList[3]].addParcelToCraft(cargoListId[parcel].weight, cargoListId[parcel].volume)
+            space3.append(parcel)
+            packetCount += 1
+        elif spaceCraftId[spacecraftList[4]].checkFitCraft(cargoListId[parcel].weight, cargoListId[parcel].volume) != False:
+            spaceCraftId[spacecraftList[4]].addParcelToCraft(cargoListId[parcel].weight, cargoListId[parcel].volume)
+            space4.append(parcel)
+            packetCount += 1
+        elif spaceCraftId[spacecraftList[5]].checkFitCraft(cargoListId[parcel].weight, cargoListId[parcel].volume) != False:
+            spaceCraftId[spacecraftList[5]].addParcelToCraft(cargoListId[parcel].weight, cargoListId[parcel].volume)
+            space5.append(parcel)
+            packetCount += 1
+        else:
+            parcelList.append(parcel)
+            break
+
+    parceldict = {spacecraftList[0]:space0, spacecraftList[1]:space1, spacecraftList[2]:space2, spacecraftList[3]:space3, spacecraftList[4]:space4, spacecraftList[5]:space5}
+    aantalparcels = {spacecraftList[0]:len(space0), spacecraftList[1]:len(space1), spacecraftList[2]:len(space2), spacecraftList[3]:len(space3), spacecraftList[4]:len(space4), spacecraftList[5]:len(space5)}
+    weight = {}
+    volume = {}
+    price = {}
+    runprice = 0
+    for spacecraft in spaceCraftId.keys():
+        weight[spacecraft] = spaceCraftId[spacecraft].currentPayloadMass
+        volume[spacecraft] = spaceCraftId[spacecraft].currentPayload
+        ftw = spaceCraftId[spacecraft].fuelToWeight
+        spaceCraftId[spacecraft].calculateFuel(ftw)
+        if spaceCraftId[spacecraft].currentPayloadMass != 0 and spaceCraftId[spacecraft].currentPayload != 0:
+            price[spacecraft] =  spaceCraftId[spacecraft].calculateCost(spaceCraftId[spacecraft].calculateFuel())
+            runprice += spaceCraftId[spacecraft].calculateCost(spaceCraftId[spacecraft].calculateFuel())
+        else:
+            price[spacecraft] = 0
+            runprice += 0
+    returndict = {"Parcellists":parceldict, "NumberOfParcels":aantalparcels, "weight":weight, "volume":volume, "price":price}
+    return returndict, parcelList, runprice
+randomAlgorithmD(10000000);
