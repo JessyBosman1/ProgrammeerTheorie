77cda76936332d582526d2968ab7b10c008ae394
Vincent Damen <vinster1997@gmail.com>
2017-12-18 15:38:11 +0100

    made the ultimate hillclimber for D

--- scripts/D/optimalHillClimberD.py
+++ scripts/D/optimalHillClimberD.py
@@ -6,20 +6,34 @@ import main
 import csv
 
 
-
 if __name__ == '__main__':
+	spaceCraftId = main.createObjectsSpaceCraft("DE")
+	cargoListName = "SwitchD"
+	cargoListIdOld = main.createObjectsCargoList(3)
+	startFile = "OptimalRandomList.csv"
+	FilledInParcels = []
+	usedParcels = []
+	unusedParcels = supportHC.MakeTemporaryCargoList(cargoListIdOld,"resultsFirstHalf.csv",cargoListName,0,True)
+	reDoFile = "Used.csv"
+	for x in range(1,2):
+		supportHC.MakeTemporaryCargoList(cargoListIdOld,"resultsFirstHalf.csv",cargoListName,x)
+		cargoListId = main.createObjectsCargoList(cargoListName)
+		supportHC.generateRandomList(startFile,cargoListId,spaceCraftId)
+		saveFile = "OptimalFirstRun"+str(x)+".csv"
+		usedParcels, unusedAttempt = supportHC.hillClimber(startFile, "OptimalResultsD.csv", saveFile, cargoListId, spaceCraftId, [], False, 5, 100000, 20, 15, True, True)
+		unusedParcels = unusedParcels + unusedAttempt
+		FilledInParcels.append(usedParcels)
+	supportHC.SaveToRetry(reDoFile,FilledInParcels)
+	# Fitting in the remaining Parcels
+	for x in range(1,2):
+		supportHC.MakeTemporaryCargoList(cargoListIdOld,reDoFile,cargoListName,x,False,unusedParcels)
+		cargoListId = main.createObjectsCargoList(cargoListName)
+		supportHC.generateRandomList(startFile,cargoListId,spaceCraftId)
+		unusedParcels = supportHC.hillClimber(startFile, "OptimalResultsD.csv", "OptimalFinalResulsts.csv", cargoListId, spaceCraftId, [], False, 5, 100000, 20, 15, True, True)[1]
 
 
 
-def MakeTemporaryCargoList(oldCargolistId, filenameSendings, outputFilename,wantedShipment):
-	sending = supportHC.openResults(oldCargolistId)[1][wantedShipment]
-	CurrentCargoList = '../../data/' + outputFilename + '.csv'
-	with open(CurrentCargoList, "w", newline='') as file:
-		spamwriter = csv.writer(file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-		spamwriter.writerow(['parcel_ID','weight (kg)','volume (m^3)'])
-		for item in sending:
-			stats = []
-			stats.append(item)
-			stats.append(oldCargolistId[item].weight)
-			stats.append(oldCargolistId[item].volume)
-			spamwriter.writerow(stats)
+
+
+
+
